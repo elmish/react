51954aeb1c3c2d3819c6218e85a8be9547586da6
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-11-18 12:38:34 +0100

    Add dispatch function to root state

--- src/react.fs
+++ src/react.fs
@@ -18,31 +18,35 @@ module Program =
                 { new IDisposable with
                     member __.Dispose() = listener <- None }
 
-    type [<Pojo>] private Props<'S, 'Msg> = {
-        view: 'S -> ('Msg->unit) -> ReactElement
-        dispatch: 'Msg->unit
-        observable: IObservable<'S>
-    }
+    type [<Pojo>] private Props<'S, 'Msg> =
+        { view: 'S -> ('Msg->unit) -> ReactElement
+          observable: IObservable<'S * ('Msg->unit)>
+          initState: 'S * ('Msg->unit) }
 
-    type [<Pojo>] private State<'T> = { value: 'T }
+    type [<Pojo>] private State<'S, 'Msg> =
+        { model: 'S
+          dispatch: 'Msg->unit }
 
-    type private ReactCom<'S, 'Msg>(initProps, initState) =
-        inherit Component<Props<'S, 'Msg>, State<'S>>(initProps)
+    type private ReactCom<'S, 'Msg>(p) =
+        // Don't keep properties to release memory of initial state
+        inherit Component<obj, State<'S, 'Msg>>(null)
+        let view = p.view
+        let obs = p.observable
         let mutable disp = None
-        do base.setInitState { value = initState }
+        do base.setInitState { model = fst p.initState; dispatch = snd p.initState }
 
         member this.componentDidMount() =
-            disp <- Some (this.props.observable.Subscribe(fun model ->
-                this.setState({ value = model })))
+            disp <- Some (obs.Subscribe(fun (model, dispatch) ->
+                this.setState { model = model; dispatch = dispatch }))
 
         member __.componentWillUnmount() =
             disp |> Option.iter (fun d -> d.Dispose())
 
         member this.shouldComponentUpdate(_, nextState) =
-            not(obj.ReferenceEquals(this.state.value, nextState.value))
+            not(obj.ReferenceEquals(this.state.model, nextState.model))
 
         member this.render() =
-            this.props.view this.state.value this.props.dispatch
+            view this.state.model this.state.dispatch
 
     open Fable.Import.Browser
 
@@ -59,13 +63,13 @@ module Program =
                 | Some r -> window.cancelAnimationFrame r
                 | None -> ()
                 lastRequest <- Some (window.requestAnimationFrame (fun _ ->
-                    observable.Trigger(model)))
+                    observable.Trigger(model, dispatch)))
             | None ->
                 let domEl = document.getElementById(placeholderId)
                 let reactEl = R.com<ReactCom<_,_>,_,_>
                                 { view = program.view
-                                  dispatch = dispatch
-                                  observable = observable } []
+                                  observable = observable
+                                  initState = (model, dispatch) } []
                 rootEl <- Some reactEl
                 Fable.Import.ReactDom.render(reactEl, domEl)
 
