e19d06cb2efbdb2acbe86bc6ac38d4ff7e52fd7d
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-11-22 11:31:06 +0100

    Some improvements after testing

--- src/react.fs
+++ src/react.fs
@@ -21,56 +21,53 @@ module Program =
     type [<Pojo>] private Props<'S, 'Msg> =
         { view: 'S -> ('Msg->unit) -> ReactElement
           observable: IObservable<'S * ('Msg->unit)>
-          initState: 'S * ('Msg->unit) }
+          getInitState: unit -> ('S * ('Msg->unit)) }
 
     type [<Pojo>] private State<'S, 'Msg> =
         { model: 'S
           dispatch: 'Msg->unit }
 
     type private ReactCom<'S, 'Msg>(p) =
-        // Don't keep properties to release memory of initial state
-        inherit Component<obj, State<'S, 'Msg>>(null)
-        let view = p.view
-        let obs = p.observable
+        inherit Component<Props<'S, 'Msg>, State<'S, 'Msg>>(p)
         let mutable disp = None
-        do base.setInitState { model = fst p.initState; dispatch = snd p.initState }
+        let model, dispatch = p.getInitState()
+        do base.setInitState { model = model; dispatch = dispatch }
 
         member this.componentDidMount() =
-            disp <- Some (obs.Subscribe(fun (model, dispatch) ->
+            disp <- Some (this.props.observable.Subscribe(fun (model, dispatch) ->
                 this.setState { model = model; dispatch = dispatch }))
 
         member __.componentWillUnmount() =
             disp |> Option.iter (fun d -> d.Dispose())
 
+        // In DEBUG mode, HMR may be activated so we need
+        // to update the state every time
+        #if !DEBUG
         member this.shouldComponentUpdate(_, nextState) =
             not(obj.ReferenceEquals(this.state.model, nextState.model))
+        #endif
 
         member this.render() =
-            view this.state.model this.state.dispatch
+            this.props.view this.state.model this.state.dispatch
 
     open Fable.Import.Browser
 
     /// Setup rendering of root React component inside html element identified by placeholderId
     let withReact placeholderId (program:Elmish.Program<_,_,_,_>) =
-        let mutable rootEl = None
-        let mutable lastRequest = None
-        let observable = SingleObservable()
+        let mutable observable: SingleObservable<_> option = None
 
         let setState model dispatch =
-            match rootEl with
-            | Some _ ->
-                match lastRequest with
-                | Some r -> window.cancelAnimationFrame r
-                | None -> ()
-                lastRequest <- Some (window.requestAnimationFrame (fun _ ->
-                    observable.Trigger(model, dispatch)))
+            match observable with
+            | Some obs ->
+                obs.Trigger(model, dispatch)
             | None ->
+                let obs = SingleObservable()
+                observable <- Some obs
                 let domEl = document.getElementById(placeholderId)
                 let reactEl = R.com<ReactCom<_,_>,_,_>
                                 { view = program.view
-                                  observable = observable
-                                  initState = (model, dispatch) } []
-                rootEl <- Some reactEl
+                                  observable = obs
+                                  getInitState = fun () -> (model, dispatch) } []
                 Fable.Import.ReactDom.render(reactEl, domEl)
 
         { program with setState = setState }
