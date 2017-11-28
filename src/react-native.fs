95c1342008c2816ec681714287be1fec4fb69209
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-11-28 10:49:06 +0100

    Update to Fable.React 1.3

--- src/react-native.fs
+++ src/react-native.fs
@@ -6,7 +6,7 @@ open Fable.Core
 open Elmish
 
 module Components =
-    type [<Pojo>] AppState = { 
+    type [<Pojo>] AppState = {
         render : unit -> ReactElement
         setState : AppState -> unit
     }
@@ -22,18 +22,18 @@ module Components =
                 this.setInitState state
             | _ -> failwith "was Elmish.ReactNative.Program.withReactNative called?"
 
-        member this.componentDidMount() =
+        override this.componentDidMount() =
             appState <- Some { appState.Value with setState = this.setState }
 
-        member this.componentWillUnmount() =
+        override this.componentWillUnmount() =
             appState <- Some { appState.Value with setState = ignore; render = this.state.render }
 
-        member this.render () = 
+        override this.render () =
             this.state.render()
 
-[<Import("AppRegistry","react-native")>] 
+[<Import("AppRegistry","react-native")>]
 type AppRegistry =
-    static member registerComponent(appKey:string, getComponentFunc:unit->ComponentClass<_>) : unit = 
+    static member registerComponent(appKey:string, getComponentFunc:unit->ComponentClass<_>) : unit =
         failwith "JS only"
 
 [<RequireQualifiedAccess>]
@@ -47,9 +47,9 @@ module Program =
         AppRegistry.registerComponent(appKey, fun () -> unbox typeof<App>)
         let render m d =
              match appState with
-             | Some state -> 
+             | Some state ->
                 state.setState { state with render = fun () -> program.view m d }
-             | _ -> 
-                appState <- Some { render = fun () -> program.view m d 
+             | _ ->
+                appState <- Some { render = fun () -> program.view m d
                                    setState = ignore }
         { program with setState = render }
