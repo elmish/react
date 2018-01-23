71fa3f60e61b1cb941c8d988d32871725f1eeeca
Eugene Tolmachev <eugene.tolmachev@prolucid.ca>
2018-01-23 08:25:41 -0500

    updated React bindings to stable 2.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,4 @@
-### 1.0.1-beta-1
+### 1.0.1
 
 * Use new React bindings, various text input jumping cursor workarounds
 
--- docs/content/react.fsx
+++ docs/content/react.fsx
@@ -14,11 +14,21 @@ namespace Elmish.React
 open System
 open Fable.Import.React
 open Fable.Core
+open Fable.Helpers.React
+
+[<AutoOpen>]
+module Helpers =
+    open Fable.Helpers.React.Props
+    open Fable.Core.JsInterop
+
+    /// `Ref` callback that sets the value of an input textbox after DOM element is created.
+    /// Can be used override input box value.
+    let inline valueOrDefault value =
+        Ref <| (fun e -> if e |> isNull |> not && !!e?value <> !!value then e?value <- !!value)
 
 [<RequireQualifiedAccess>]
 module Program =
     open Fable.Import.Browser
-    module R = Fable.Helpers.React
 
     /// Setup rendering of root React component inside html element identified by placeholderId
     let withReact placeholderId (program:Elmish.Program<_,_,_,_>) =
@@ -28,10 +38,21 @@ module Program =
             | Some r -> window.cancelAnimationFrame r
             | _ -> ()
 
-            lastRequest <- Some (window.requestAnimationFrame (fun _ -> 
+            lastRequest <- Some (window.requestAnimationFrame (fun _ ->
                 Fable.Import.ReactDom.render(
                     lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
                     document.getElementById(placeholderId)
                 )))
 
-        { program with setState = setState } 
+        { program with setState = setState }
+
+    /// `withReact` uses `requestAnimationFrame` to optimize rendering in scenarios with updates at a higher rate than 60FPS, but this makes the cursor jump to the end in `input` elements.
+    /// This function works around the glitch if you don't need the optimization (see https://github.com/fable-elmish/react/issues/12).
+    let withReactUnoptimized placeholderId (program:Elmish.Program<_,_,_,_>) =
+        let setState model dispatch =
+            Fable.Import.ReactDom.render(
+                lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
+                document.getElementById(placeholderId)
+            )
+
+        { program with setState = setState }
