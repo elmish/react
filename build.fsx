123b333cd1e091460c7aa3fb6a24b02d0153a652
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2022-09-09 11:40:02 +0900

    Fix build script

--- build.fsx
+++ build.fsx
@@ -48,7 +48,8 @@ Target.create "Build" (fun _ ->
 
 let release = ReleaseNotes.load "RELEASE_NOTES.md"
 
-Target.create "Meta" (fun _ -> $"""
+Target.create "Meta" (fun _ ->
+    $"""
 <Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
   <ItemGroup>
     <None Include="$(MSBuildThisFileDirectory)/docs/static/img/logo.png" Pack="true" PackagePath="\" />
@@ -61,7 +62,7 @@ Target.create "Meta" (fun _ -> $"""
     <AllowedOutputExtensionsInPackageBuildOutputFolder>$(AllowedOutputExtensionsInPackageBuildOutputFolder);.pdb</AllowedOutputExtensionsInPackageBuildOutputFolder>
     <Description>Elmish extensions for writing Fable apps with React and ReactNative</Description>
     <PackageProjectUrl>http://{gitOwner}.github.io/{gitName}</PackageProjectUrl>
-    <PackageLicenseFile>LICENSE</PackageLicenseFile>
+    <PackageLicenseFile>LICENSE.md</PackageLicenseFile>
     <PackageReadmeFile>README.md</PackageReadmeFile>
     <PackageIcon>logo.png</PackageIcon>
     <RepositoryUrl>{gitHome}/{gitName}</RepositoryUrl>
